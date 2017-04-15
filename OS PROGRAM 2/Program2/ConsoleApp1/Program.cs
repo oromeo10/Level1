268fbffd61eceeff7387de0af887d7839b2805b2
rocampo3 <rocampo3@student.gsu.edu>
2017-04-15 00:41:27 -0400

    start of console progr2

---
+++ OS PROGRAM 2/Program2/ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 3;
+            int b = 4;
+            for (int i = 0; i <= 10; i++) {
+                Console.WriteLine("Your result is: " + (a + b + i)) ;
+                
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
