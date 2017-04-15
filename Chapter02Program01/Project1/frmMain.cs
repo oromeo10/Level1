bfa750408f3f5cb63a6a21349660e4cb2e6cf24f
rocampo3 <rocampo3@student.gsu.edu>
2017-04-14 23:06:25 -0400

    initiation of learning in c sharp

---
+++ Chapter02Program01/Project1/frmMain.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Windows.Forms;
+public class frmMain : Form
+{
+    private TextBox textBox1;
+    private Button button1;
+    private Label label1;
+    #region Windows Code
+    private void InitializeComponent()
+    {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(58, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 23);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Name:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(97, 40);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(197, 37);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // frmMain
+            // 
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "frmMain";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+    }
+    #endregion
+    public frmMain()
+    {
+        InitializeComponent();
+    }
+    [STAThread]
+    public static void Main()
+    {
+        frmMain main = new frmMain();
+        Application.Run(main);
+    }
+
+    private void button1_Click(object sender, EventArgs e)
+    {
+        frmMain.ActiveForm.Text = textBox1.Text;
+    }
+}
